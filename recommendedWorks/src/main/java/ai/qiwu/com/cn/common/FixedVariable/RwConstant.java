a870584da9880cc6f63153ebd463f5dd628d93bb
huangganda <1638049431@qq.com>
2021-01-16 14:18:51 +0800

    手表推荐V1.1

--- recommendedWorks/src/main/java/ai/qiwu/com/cn/common/FixedVariable/RwConstant.java
+++ recommendedWorks/src/main/java/ai/qiwu/com/cn/common/FixedVariable/RwConstant.java
@@ -9,10 +9,10 @@ public interface RwConstant {
      * 接口地址
      */
     interface UrlInterface{
-        String QI_WU_RECOMMEND = "http://hw-gz24.heyqiwu.cn:18082/api/data/cache";
-        String QI_WU_BOTCONFIG = "http://hw-gz24.heyqiwu.cn:8070/api/sdk/botConfig/all";
-        /*String QI_WU_RECOMMEND = "http://localhost:18082/api/data/cache";
-        String QI_WU_BOTCONFIG = "http://localhost:8070/api/sdk/botConfig/all";*/
+        /*String QI_WU_RECOMMEND = "http://hw-gz24.heyqiwu.cn:18082/api/data/cache";
+        String QI_WU_BOTCONFIG = "http://hw-gz24.heyqiwu.cn:8070/api/sdk/botConfig/all";*/
+        String QI_WU_RECOMMEND = "http://localhost:18082/api/data/cache";
+        String QI_WU_BOTCONFIG = "http://localhost:8070/api/sdk/botConfig/all";
     }
 
 }
